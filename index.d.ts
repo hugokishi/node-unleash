c61078389d93a12bf09d6962e3b906b68d2596cc
hugokishi <hugopacheco370@gmail.com>
2023-01-19 23:56:36 -0300

    added types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,33 @@
+export interface Properties {
+  [key: string]: string | undefined | number;
+}
+export interface Context {
+  [key: string]: string | Date | undefined | number | Properties;
+  currentTime?: Date;
+  userId?: string;
+  sessionId?: string;
+  remoteAddress?: string;
+  environment?: string;
+  appName?: string;
+  properties?: Properties;
+}
+
 /**
   * ! Use this function if you want to export the logs and warn of a possible error
   */
-export function isEnabled(flagName: string, context: Unleash.Context): boolean
+export function isEnabled(flagName: string, context: Context): boolean
 
 /**
  * ! Start a new unleash client
  */
-export function InitUnleashClient(): void
+export function InitUnleashClient({
+  url,
+  appName,
+  authorizationToken,
+  environment,
+}: {
+  url: string,
+  appName: string,
+  authorizationToken: string,
+  environment: string,
+}): void
